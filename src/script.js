2ede5ba26f73f9305c52e63d73fa77731750406f
Philipp Dikmann <mail@philippdikmann.de>
2020-09-18 12:46:43 +0200

    randomize based on where user clicks.

--- src/script.js
+++ src/script.js
@@ -1,5 +1,11 @@
 var content = document.getElementById("content")
 var randomize = document.getElementById("randomize")
+var emojis = ["👷", // occupation
+              "🏢", // building
+              "🔍", // adjective
+              "✊", // verb
+              "📃" // thing
+             ]
 var set = [1, 0, 4, 4, 4, 2, 3]
 var lengths = (() => {
   let len = []
@@ -10,37 +16,73 @@ var lengths = (() => {
 })() // unnecessary, not an optimization
 var buttons = []
 
-randomize.addEventListener("click", Random)
+randomize.addEventListener("click", RandomizeAll)
 
-function Random() {
-  Clear()
+function InitialSet() {
+  ClearButtons()
   for (i of set) {
     WriteButton(i)
   }
 }
 
 function WriteButton(i) {
-  let f = fields[i]
   let b = document.createElement("button")
-  b.addEventListener("click", () => RandomizeButton(b, f))
-  RandomizeButton(b, f)
+  b.fieldIndex = i;
+  RandomizeButtonContent(b)
   content.appendChild(b)
   buttons.push(b)
-}
-
-function RandomizeButton(b, f) {
-  b.textContent = f.data[Math.floor(Math.random() * f.data.length)]
 }
 
 function WriteLine(str) {
   content.innerHTML = content.innerHTML + str + "<br>"
 }
 
-function Clear() {
+function RandomizeAll() {
+  for (b of buttons) {
+    RandomizeButtonContent(b)
+  }
+}
+
+function RandomizeButton(b) {
+  let i = Math.floor(Math.random() * fields.length)
+  b.fieldIndex = i
+  RandomizeButtonContent(b)
+}
+
+function RandomizeButtonContent(b) {
+  let f = fields[b.fieldIndex]
+  ClearElement(b)
+  let type = MakeElement("div", "type", emojis[b.fieldIndex])
+  type.addEventListener("click", () => ToggleButtonType(b))
+  b.appendChild(type)
+  let content = MakeElement("div", "label", f.data[Math.floor(Math.random() * f.data.length)])
+  content.addEventListener("click", () => RandomizeButtonContent(b))
+  b.appendChild(content)
+}
+
+function ToggleButtonType (b) {
+  b.fieldIndex = (b.fieldIndex + 1) % fields.length
+  RandomizeButtonContent(b)
+}
+
+function MakeElement(element, className, content) {
+  let e = document.createElement(element)
+  e.className = className
+  e.textContent = content
+  return e
+}
+
+function ClearButtons() {
   for (b of buttons) {
     content.removeChild(b)
   }
   buttons = []
 }
 
-Random()
+function ClearElement(e) {
+  while (e.firstChild) {
+    e.removeChild(e.lastChild)
+  }
+}
+
+InitialSet()
