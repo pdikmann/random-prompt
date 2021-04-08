9c1994499aa910165847c55917f515c8e1aa03c3
Philipp Dikmann <mail@philippdikmann.de>
2021-04-08 14:33:12 +0200

    expose RunTabChangeHook

--- src/tabs.js
+++ src/tabs.js
@@ -1,6 +1,7 @@
 var tabs = document.getElementsByClassName("tab")
 var tabbuttons = document.getElementById("tabbar").children
 var onTabChangeHooks = []
+var currentTab = 0
 
 for (let i = 0; i < tabbuttons.length; i++) {
   tabbuttons[i].addEventListener("click", () => ShowTab(i))
@@ -19,6 +20,11 @@ function ShowTab(n) {
   //tabs[n].className = "tab"
   RemoveClass(tabs[n], "hidden")
   AddClass(tabbuttons[n], "active")
+  RunTabChangeHook(n)
+  currentTab = n
+}
+
+function RunTabChangeHook(n) {
   if (typeof onTabChangeHooks[n] == "function") onTabChangeHooks[n]()
 }
 
